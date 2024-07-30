4425fa78c53645996b41ce75076393ae681075c3
Joseph Pal <josephpal27@gmail.com>
2024-07-30 14:53:26 +0530

    making responsive

--- script.js
+++ script.js
@@ -41,6 +41,78 @@ document.addEventListener('click', () => {
 toggleContentBoxes();
 
 
+// ---------------------------------------------------------
+
+// Functionality for Nav Menu Toggle
+let menuBtn = document.querySelector('.nav-menu ul .fa-bars');
+let navMenu2 = document.querySelector('.nav-menu-2');
+
+menuBtn.addEventListener('click', () => {
+    if (navMenu2.style.opacity == '1') {
+        navMenu2.style.height = '0';
+        navMenu2.style.opacity = '0';
+        navMenu2.style.zIndex = '-10';
+
+    } else {
+        navMenu2.style.opacity = '1';
+        navMenu2.style.zIndex = '10';
+        navMenu2.style.height = 'auto';
+    }
+})
+
+
+let aboutUs = document.getElementById('about-us');
+let aboutUsContent = document.getElementById('about-us-content');
+
+aboutUs.addEventListener('click', () => {
+    if (aboutUsContent.style.display == 'block') {
+        // aboutUsContent.style.height = '0';
+        // aboutUsContent.style.zIndex = '-10';
+        // aboutUsContent.style.opacity = '-10';
+        aboutUsContent.style.display = 'none';
+    } else {
+        // aboutUsContent.style.height = 'auto';
+        // aboutUsContent.style.zIndex = '10';
+        // aboutUsContent.style.opacity = '10';
+        aboutUsContent.style.display = 'block';
+    }
+})
+
+let companies = document.getElementById('companies');
+let companiesContent = document.getElementById('companies-content');
+
+companies.addEventListener('click', () => {
+    if (companiesContent.style.display == 'block') {
+        // companiesContent.style.height = '0';
+        // companiesContent.style.zIndex = '-10';
+        // companiesContent.style.opacity = '-10';
+        companiesContent.style.display = 'none';
+    } else {
+        // companiesContent.style.height = 'auto';
+        // companiesContent.style.zIndex = '10';
+        // companiesContent.style.opacity = '10';
+        companiesContent.style.display = 'block';
+    }
+})
+
+let research = document.getElementById('research');
+let researchContent = document.getElementById('research-content');
+
+research.addEventListener('click', () => {
+    if (researchContent.style.display == 'block') {
+        // companiesContent.style.height = '0';
+        // companiesContent.style.zIndex = '-10';
+        // companiesContent.style.opacity = '-10';
+        researchContent.style.display = 'none';
+    } else {
+        // companiesContent.style.height = 'auto';
+        // companiesContent.style.zIndex = '10';
+        // companiesContent.style.opacity = '10';
+        researchContent.style.display = 'block';
+    }
+})
+
+
 // ------------------------------------------------------------------------------------------------------------------------------
 
 // Functionality For Back To Top Button
